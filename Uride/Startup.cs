6724b6590011457f46190c0fc254a5f43a5cbe21
David-PC <dk9946@student.uni-lj.si>
2019-01-08 14:18:30 +0100

    maybe now?

--- Uride/Startup.cs
+++ Uride/Startup.cs
@@ -58,7 +58,13 @@ namespace Uride
             services.Configure<RequestLocalizationOptions>(
                options =>
                {
+                   var supportedCultures = new List<CultureInfo>
+                   {
+                       new CultureInfo("en-US"),
+                   };
                    options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+                   options.SupportedCultures = supportedCultures;
+                   options.SupportedUICultures = supportedCultures;
                });
 
             services.AddDbContext<ApplicationDbContext>(options =>
